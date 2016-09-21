46cf3357cb11cc7bd53e2140281bde505ea3a9ae
AsimoLoveGym <xiazhuangz@gmail.com>
2016-09-21 03:18:43 -0500

    feat: for Closed Account, will be displayed
    Todo: Customize for more channels, comments on code, ReadMe file for gitHub, codepen version, filter function

--- js/main.js
+++ js/main.js
@@ -10,7 +10,11 @@ var Channel = function(item) {
 
 var channels = [];
 
-var channelsList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "sodapoppin", "moonducktv"];
+var img_placeholder = "https://www.acspri.org.au/sites/acspri.org.au/files/profile-placeholder.png";
+var url_placeholder = "https://www.twitch.tv/";
+var status_placeholder = "Closed";
+
+var channelsList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "sodapoppin", "moonducktv","fskdgjkdsjgfk"];
 
 for (var i=0; i < channelsList.length; i++) {
   // console.log(i);
@@ -50,7 +54,7 @@ for (var i=0; i < channelsList.length; i++) {
         + '<div class="col-xs-12 vertical-center-text collapse" id="' + channelInfo.name.toLowerCase() + '">'
         + '<div class="col-xs-12 panel panel-primary"><div class="panel-heading">Game: ' + channelInfo.game + '</div>'
         + '<div class="panel-body bg-info text-center"><h5 class="text-center">' + channelInfo.stream_status + '</h5>'
-        + '<h6 class="text-right">' + channelInfo.followers + ' Followers</h6><h6 class="text-right">' + channelInfo.views + ' Watching</h6>'
+        + '<h6 class="text-right">' + numberWithCommas(channelInfo.followers) + ' Followers</h6><h6 class="text-right">' + numberWithCommas(channelInfo.views) + ' Watching</h6>'
         + '<a href="' + channelInfo.video_url + '" target="_blank" class="btn btn-primary" role="button">Watch</a></div></div></div></div>';
         // $("#online").append(addItem);
         // id using limitation for collapse targeting, use a special one for online case
@@ -71,7 +75,7 @@ for (var i=0; i < channelsList.length; i++) {
         + '<div class="col-xs-12 vertical-center-text collapse" id="' + channelInfo.name.toLowerCase() + channelInfo.channel_status + '">'
         + '<div class="col-xs-12 panel panel-primary"><div class="panel-heading">Game: ' + channelInfo.game + '</div>'
         + '<div class="panel-body bg-info text-center"><h5 class="text-center">' + channelInfo.stream_status + '</h5>'
-        + '<h6 class="text-right">' + channelInfo.followers + ' Followers</h6><h6 class="text-right">' + channelInfo.views + ' Watching</h6>'
+        + '<h6 class="text-right">' + numberWithCommas(channelInfo.followers) + ' Followers</h6><h6 class="text-right">' + numberWithCommas(channelInfo.views) + ' Watching</h6>'
         + '<a href="' + channelInfo.video_url + '" target="_blank" class="btn btn-primary" role="button">Watch</a></div></div></div></div>';
         $("#online").append(addItem);
      }
@@ -87,7 +91,22 @@ for (var i=0; i < channelsList.length; i++) {
     //  } else {
     //    $("#offline").append(addItem);
     //  }
-   }
+  },
+  error: function(data) {
+    console.log(data.responseText);
+    var responseText = data.responseText;
+    var channel_name = responseText.substring(responseText.indexOf("'")+1,responseText.lastIndexOf("'"));
+    // console.log(channel_name);
+    // var channel_name = responseText.split(/[\'\']/);
+    // console.log(channel_name);
+    var addItem = '<div class="col-xs-12 channel-list"><figure class="col-xs-2"><img src="' + img_placeholder + '" alt="channel photo" class="img-circle channel-photo center-block">'
+        + '</figure><a href="' + url_placeholder + '" target="_blank" class="col-xs-8 text-center vertical-center-text">' + channel_name + '</a><div class="col-xs-2 vertical-center-text">'
+        + '<span class="badge badge-danger">' + status_placeholder + '</span></div></div>';
+
+    $("#all").append(addItem);
+
+    console.log(data);
+  }
   });
 }
 
